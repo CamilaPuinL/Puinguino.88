123843f8d2b12f83ce7d1ed1bb49018619816eef
CamilaPuinL <camilapuinloaiza@gmail.com>
2026-03-02 16:48:29 +0000

    Cambio

---
+++ script.js
@@ -0,0 +1,8 @@
+//console.log("hola");
+const varTitulo = document.getElementById (titulo);
+
+console.log(varTitulo)
+varTitulo.addEventListener("click", ()=>{
+    varTitulo.innerText = "Buenos dias"
+    varTitulo.style.background
+})
